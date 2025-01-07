ffc484ad2909d25b548b65b978474eb1f37a9813
StefanoDaniello <stefanodaniello68@gmail.com>
2025-01-07 15:14:57 +0100

    add appwrite

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,68 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
+import { login } from "@/lib/appwrite";
 
 const SignIn = () => {
+  const handleLogin = async () => {
+    const result = await login();
+    if (result) {
+      console.log("Login success");
+    } else {
+      Alert.alert("Login failed");
+    }
+  };
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    // La SafeAreaView serve a visualizzare in modo sicuro il contenuto in modo che si adatti a qualsiasi tipo di schermo
+    <SafeAreaView className="bg-white h-full">
+      {/* ScrollView serve a visualizzare il contenuto in modo scorrevole */}
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome To Real Scout
+          </Text>
+
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's Get You Closer To {"\n"}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
+
+          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+            Login to Real Scout with Google
+          </Text>
+
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
