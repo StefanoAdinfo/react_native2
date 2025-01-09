1eef90c344ff1308d8137bfa31d55749a7da4208
StefanoDaniello <stefanodaniello68@gmail.com>
2025-01-09 11:11:18 +0100

    add properties function

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -94,3 +94,59 @@ export async function getCurrentUser() {
     return null;
   }
 }
+
+export async function getLatestProperties() {
+  try {
+    const result = await databases.listDocuments(
+      //il ! va a significare che proviende dall' env
+      config.databaseId!,
+      config.propertiesCollectionId!,
+      [Query.orderAsc("$createdAt"), Query.limit(5)]
+    );
+    return result.documents;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function getProperties({
+  filter,
+  query,
+  limit,
+}: {
+  filter: string;
+  query: string;
+  limit?: number;
+}) {
+  try {
+    const buildQuery = [Query.orderDesc("$createdAt")];
+    if (filter && filter !== "All") {
+      buildQuery.push(Query.equal("type", filter));
+    }
+    if (query) {
+      buildQuery.push(
+        Query.or([
+          Query.search("name", query),
+          Query.search("address", query),
+          Query.search("type", query),
+        ])
+      );
+    }
+
+    if (limit) {
+      buildQuery.push(Query.limit(limit));
+    }
+
+    const result = await databases.listDocuments(
+      //il ! va a significare che proviende dall' env
+      config.databaseId!,
+      config.propertiesCollectionId!,
+      buildQuery
+    );
+    return result.documents;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
